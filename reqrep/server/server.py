9f5bb684e8f7eac195f456c9e51997b77b1e2a9c
adriano-229 <fabris.adriano@uncuyo.edu.ar>
2026-04-19 18:24:10 -0300

    added written part

---
+++ reqrep/server/server.py
@@ -0,0 +1,28 @@
+#
+#   Hello World server in Python
+#   Binds REP socket to tcp://*:5555
+#   Expects b"Hello" from client, replies with b"World"
+#
+
+import time
+
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+print("Server initialized.")
+
+while True:
+    #  Wait for next request from client
+    message = socket.recv()
+    print("Received request: %s" % message)
+
+    if message != "Hello":
+        socket.send("Decí Hello, error")
+    else:
+        #  Do some 'work'
+        time.sleep(1)
+
+        #  Send reply back to client
+        socket.send(b"World")
